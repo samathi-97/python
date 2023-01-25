893a533c1dbad743ca213e0e1bc1b2a60c85d5aa
samathi <samathisapumana@gmail.com>
2023-01-25 11:31:12 -0800

    changes

--- dictionaryInPython.py
+++ dictionaryInPython.py
@@ -1,3 +1,8 @@
 person = {"firstName": "Samathi","age": 23 , "Gender":"female" , "Addres": "Tangalle"}
 
 print(person["firstName"])
+
+print("What you want to know")
+key = input()
+
+print(person.get(key))
