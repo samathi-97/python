707edb6c005e74181f0ce2a8250a37ebce249a65
samathi <samathisapumana@gmail.com>
2023-01-24 20:50:16 -0800

    elif

--- firstPythonPro.py
+++ firstPythonPro.py
@@ -1,7 +1,17 @@
 print(5+6)
-
+# if else in python
 x = 4
 if x == 5:
    print("Yes")
 else:
    print("No")
+# shorthanf if/else -> elif
+num = 3
+if num == 1:
+  print("One")
+elif num == 2:
+  print("Two")
+elif num == 3: 
+  print("Three")
+else: 
+  print("Something else")
