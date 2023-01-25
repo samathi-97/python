6745a03dc16db88f8328787a634ac63340b3f53e
samathi <samathisapumana@gmail.com>
2023-01-24 20:55:59 -0800

    leap year program

--- firstPythonPro.py
+++ firstPythonPro.py
@@ -15,3 +15,17 @@ elif num == 3:
   print("Three")
 else: 
   print("Something else")
+
+# code to finf leap year or not
+print("Enter any year")
+year = int(input())
+
+if year%4 != 0 :
+    print("not a leap year")
+elif year % 100 !=0:
+    print("Leap year")
+elif year % 400 == 0 :   
+    print("Leap year")
+elif year % 400 !=0:
+    print("not a leap year")
+    
