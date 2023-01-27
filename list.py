5640357036a9a13223f21e2e9d29231f868bbe5a
samathi <samathisapumana@gmail.com>
2023-01-26 23:55:30 -0800

    command in list program

--- list.py
+++ list.py
@@ -6,3 +6,22 @@ words = ["spam", "egg", "spam", "sausage"]
 print("spam" in words)
 print("egg" in words)
 print("tomato" in words)
+
+print("using not operator")
+
+nums = [1, 2, 3]
+print(not 4 in nums)
+print(4 not in nums)
+print(not 3 in nums)
+print(3 not in nums)
+
+print(".....Commands....")
+commands = ["Lights Off", "Lock the door", "Open the door", "Make Coffee", "Shut down"]
+#your code goes here
+com = input("Say your command:")
+bol = com in commands
+#print(com in commands)
+if ( bol == True):
+    print("OK")
+else:
+    print("Not supported")   
