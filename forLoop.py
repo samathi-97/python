e2fcc5ed44e2bb7b9dc9b718ad924ede7c8dbe8d
samathi <samathisapumana@gmail.com>
2023-01-27 00:07:36 -0800

    no of letter in sentence by for loop

--- forLoop.py
+++ forLoop.py
@@ -6,3 +6,12 @@ letters = ['a', 'b', 'c']
 
 for l in letters:
   print(l+"*")  
+
+str = "testing for loops"
+count = 0
+
+for x in str:
+  if(x == 't'):
+    count += 1
+
+print("No of t in string is :",count)   
