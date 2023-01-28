1b0dc1429cc061cbbb4c6e224f84cc54e53db24d
samathi <samathisapumana@gmail.com>
2023-01-27 20:45:21 -0800

    changes

--- forLoop.py
+++ forLoop.py
@@ -14,4 +14,18 @@ for x in str:
   if(x == 't'):
     count += 1
 
-print("No of t in string is :",count)   
+print("No of t in string is :",count) 
+
+numbers = list(range(5))
+print(numbers)
+
+numbersN = list(range(3, 8))
+print(numbersN)
+
+print(range(20) == range(0, 20))
+
+numbersM = list(range(5, 20, 2))
+print(numbersM)
+
+numbersP = list(range(20,5,-2))
+print(numbersP)
