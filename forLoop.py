3118d15c0679ca60aed72feecf264e01c456741a
samathi <samathisapumana@gmail.com>
2023-01-28 19:13:18 -0800

    join , replace,split,upper,lower

--- forLoop.py
+++ forLoop.py
@@ -64,3 +64,24 @@ for i in range(10):
 nums = [4, 5, 6]
 msg = "Numbers: {0} {1} {2}". format(nums[0], nums[1], nums[2])
 print(msg)
+
+print(", ".join(["spam", "eggs", "ham"]))
+#prints "spam, eggs, ham"
+
+print("Hello ME".replace("ME", "world"))
+#prints "Hello world"
+
+print("This is a sentence.".startswith("This"))
+# prints "True"
+
+print("This is a sentence.".endswith("sentence."))
+# prints "True"
+
+print("This is a sentence.".upper())
+# prints "THIS IS A SENTENCE."
+
+print("AN ALL CAPS SENTENCE".lower())
+#prints "an all caps sentence"
+
+print("spam, eggs, ham".split(", "))
+#prints "['spam', 'eggs', 'ham']"
